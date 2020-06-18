6bf5430b6306192298c6c4967c56a7920cdcbf1b
amanmakwana <47788196+amanmakwana@users.noreply.github.com>
2020-06-18 15:53:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_EVPcl4p64', // e.g. us-east-2_uXboG5pAb
-        region: 'us-east-1', // e.g. us-east-2
-		clientId: '3ouj8b1dtneotovct90fqh313h' //is this used anywhere?
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        region: '', // e.g. us-east-2
+		clientId: '' //is this used anywhere?
     },
 };
 
